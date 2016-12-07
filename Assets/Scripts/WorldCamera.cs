fb932271b9884f120589ac1f6d33ea58cf0f4392
brandon solis <brandon solis>
2016-12-06 19:57:44 -0800

    Fix Camera Position.

---
+++ Assets/Scripts/WorldCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 camPos = Camera.main.transform.position;
+        transform.Translate(-camPos.x, -camPos.y, -camPos.z);
+        transform.rotation.Set(0, 0, 0, 0);
+    }
+}
